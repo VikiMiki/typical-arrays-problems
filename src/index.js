6d9cf5494fd990715d295394d5dc58295e648668
VikiMiki <vikimikirc@gmail.com>
2020-03-14 02:58:01 +0300

    feat: function typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( array === undefined || array.length == 0 )
+    return 0;
+    else
+      return Math.min.apply(Math, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( array === undefined || array.length == 0 )
+    return 0;
+    else
+  return Math.max.apply(Math, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let sum=0;
+    let res=0;
+    if ( array === undefined || array.length == 0 )
+    {
+    return 0;
+    }
+    else
+    {
+    for (let i = 0; i < array.length; i++) {
+        sum=sum+array[i];
+      }
+    }
+    res=sum/array.length;
+    return res;
 }
